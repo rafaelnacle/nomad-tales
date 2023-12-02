13ac1cebb25b1164811c4ecfdf03008c77995d11
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2023-12-01 21:34:33 -0300

    style: elements size for better readability

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,24 +4,27 @@ import "../styles/Card.css";
 
 function Card(props) {
   return (
-    <article className="card">
-      <aside>
-        <img className='card--travel-img' src={props.item.imageUrl} alt="" />
-      </aside>
-      <main>
-        <div className='card--header-wrapper'>
-          <img className='card--marker-icon' src={markerIcon} alt="" />
-          <h4>{props.item.location}</h4>
-          <a href={props.item.googleMapsUrl}>View on Google Maps</a>
-        </div>
+    <>
+      <article className="card">
+        <aside>
+          <img className='card--travel-img' src={props.item.imageUrl} alt="" />
+        </aside>
+        <main>
+          <div className='card--header-wrapper'>
+            <img className='card--marker-icon' src={markerIcon} alt="" />
+            <h4>{props.item.location}</h4>
+            <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+          </div>
 
-        <h1>{props.item.title}</h1>
-        <h3>{props.item.startDate} - {props.item.endDate}</h3>
-        <p>{props.item.description}</p>
-        
-      </main>
+          <h1>{props.item.title}</h1>
+          <h3>{props.item.startDate} - {props.item.endDate}</h3>
+          <p>{props.item.description}</p>
+          
+        </main>
+      </article>
       <div className='card--separator'></div>
-    </article>
+    </>
+    
   )
 }
 
